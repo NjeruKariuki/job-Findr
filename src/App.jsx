d5fd50b4ea40a985fce9d3cdcf3698a424634dea
Chief <freakoutbond2@gmail.com>
2023-03-03 19:18:36 +0300

    changed structure of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-import Jobs from "./Pages/Jobs";
 import About from "./Pages/About";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <main className="w-screen h-screen">
+      <header className="bg-slate-800 text-white">
+        <nav>
+          <div className="Navigation h-12 shadow-md fixed w-full top-0 left-0 flex justify-between font-mono items-center px-6">
+            <h1 className="text-xl font-bold">JobFindr</h1>
+            <span className="hidden md:flex justify-between ">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="about">About Us</NavLink>
+            </span>
+            <span>
+              
+            </span>
+
+          </div>
+        </nav>
+      </header>
+      <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="jobs" element={<Jobs />} />
         </Routes>
       </main>
     </BrowserRouter>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Job from '../Components/Job';
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="h-full w-full bg-white">
+      <section className="jobSearchHeader h-48 w-screen bg-slate-700 text-white">
+        <form className="py-16 flex items-center justify-center">
+          <input className="w-auto md:w-96  border border-black rounded p-3 m-3 text-black" type="text" name="" value="" placeholder="Enter job to search for..." />
+          <button className=" md:w-36   px-5 py-3 mx-2 bg-red-700 rounded hover:bg-red-900" type="submit">Search</button>
+        </form>
+      </section>
+      <section className="jobPostings bg-slate-200 h-100vh w-screen flex flex-col items-center p-3 space-y-6 py-6">
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+        <Job />
+      </section>
     </div>
   )
 }
